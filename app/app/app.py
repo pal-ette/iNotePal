7384cbc8a19ac00293cf1194f62840361b51c5a6
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-17 15:11:23 +0900

    #35 기본 채팅 앱 구현

--- app/app/app.py
+++ app/app/app.py
@@ -1,34 +1,65 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
-from rxconfig import config
-
 import reflex as rx
 
-docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
+from app import style
+from app.state import State
 
 
-class State(rx.State):
-    """The app state."""
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.text(
+                question,
+                text_align="right",
+            ),
+            style=style.question_style,
+        ),
+        rx.box(
+            rx.text(
+                answer,
+                text_align="left",
+            ),
+            style=style.answer_style,
+        ),
+    )
+
+
+def chat() -> rx.Component:
+    return rx.box(
+        rx.foreach(
+            State.chat_history,
+            lambda messages: qa(
+                messages[0],
+                messages[1],
+            ),
+        )
+    )
+
+
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(
+            value=State.question,
+            placeholder="Ask a question",
+            on_change=State.set_question,
+            style=style.input_style,
+        ),
+        rx.button(
+            "Ask",
+            on_click=State.answer,
+            style=style.button_style,
+        ),
+    )
 
 
 def index() -> rx.Component:
     return rx.center(
-        rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            rx.logo(),
+            chat(),
+            action_bar(),
             align="center",
-            spacing="7",
-            font_size="2em",
         ),
-        height="100vh",
     )
 
 
