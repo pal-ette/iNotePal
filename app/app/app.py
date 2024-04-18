2bec72921a0bcf08082536d4b6136b0f0ef7d378
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-18 11:59:03 +0900

    #40 로컬 DB 및 로그인 인증 로직 제거
    - supabase로 대체 예정

--- app/app/app.py
+++ app/app/app.py
@@ -5,8 +5,6 @@ import reflex as rx
 from app import style
 from app.state import State
 from dotenv import load_dotenv
-from .login import require_login
-from .registration import registration_page as registration_page
 
 # 외부 정의 DB 모델
 
@@ -14,16 +12,16 @@ load_dotenv()
 
 
 def qa(question: str, answer: str) -> rx.Component:
-    return rx.box(
-        rx.box(
-            rx.text(
+    return rx.chakra.box(
+        rx.chakra.box(
+            rx.chakra.text(
                 question,
                 text_align="right",
             ),
             style=style.question_style,
         ),
-        rx.box(
-            rx.text(
+        rx.chakra.box(
+            rx.chakra.text(
                 answer,
                 text_align="left",
             ),
@@ -33,7 +31,7 @@ def qa(question: str, answer: str) -> rx.Component:
 
 
 def chat() -> rx.Component:
-    return rx.box(
+    return rx.chakra.box(
         rx.foreach(
             State.chat_history,
             lambda messages: qa(
@@ -45,14 +43,14 @@ def chat() -> rx.Component:
 
 
 def action_bar() -> rx.Component:
-    return rx.hstack(
-        rx.input(
+    return rx.chakra.hstack(
+        rx.chakra.input(
             value=State.question,
             placeholder="Ask a question",
             on_change=State.set_question,
             style=style.input_style,
         ),
-        rx.button(
+        rx.chakra.button(
             "Ask",
             on_click=State.answer,
             style=style.button_style,
@@ -60,10 +58,9 @@ def action_bar() -> rx.Component:
     )
 
 
-@require_login
 def index() -> rx.Component:
-    return rx.center(
-        rx.vstack(
+    return rx.chakra.center(
+        rx.chakra.vstack(
             chat(),
             action_bar(),
             align="center",
