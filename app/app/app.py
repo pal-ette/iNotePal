4854c34367d104b54f5a21969deaaeec39540352
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-18 12:22:25 +0900

    #40 supabase 연동

--- app/app/app.py
+++ app/app/app.py
@@ -6,6 +6,10 @@ from app import style
 from app.state import State
 from dotenv import load_dotenv
 
+from .app_state import AppState
+from .registration import registration_page as registration_page
+from .login import require_login
+
 # 외부 정의 DB 모델
 
 load_dotenv()
@@ -58,15 +62,59 @@ def action_bar() -> rx.Component:
     )
 
 
-def index() -> rx.Component:
-    return rx.chakra.center(
-        rx.chakra.vstack(
-            chat(),
-            action_bar(),
-            align="center",
+def show_logout_or_login_comp() -> rx.Component:
+    return rx.cond(
+        AppState.is_hydrated & AppState.token_is_valid,
+        rx.chakra.box(
+            rx.chakra.link("Protected Page", href="/protected", padding_right="10px"),
+            rx.chakra.link("Logout", href="/", on_click=AppState.do_logout),
+            spacing="1.5em",
+            padding_top="10%",
         ),
+        rx.chakra.box(
+            rx.chakra.link("Register", href="/register", padding_right="10px"),
+            rx.chakra.link("Login", href="/login"),
+            spacing="1.5em",
+            padding_top="10%",
+        ),
+    )
+
+
+def index() -> rx.Component:
+    """Render the index page.
+
+    Returns:
+        A reflex component.
+    """
+    return rx.fragment(
+        rx.chakra.color_mode_button(rx.chakra.color_mode_icon(), float="right"),
+        rx.chakra.vstack(
+            rx.chakra.heading("Welcome to my homepage!", font_size="2em"),
+            show_logout_or_login_comp(),
+        ),
+    )
+
+
+@require_login
+def protected() -> rx.Component:
+    """Render a protected page.
+
+    The `require_login` decorator will redirect to the login page if the user is
+    not authenticated.
+
+    Returns:
+        A reflex component.
+    """
+    return rx.chakra.vstack(
+        rx.chakra.heading("Protected Page", font_size="2em"),
+        rx.chakra.link("Home", href="/"),
+        rx.chakra.link("Logout", href="/", on_click=AppState.do_logout),
+        chat(),
+        action_bar(),
+        align="center",
     )
 
 
 app = rx.App()
 app.add_page(index)
+app.add_page(protected)
---
+++ app/app/registration.py
@@ -0,0 +1,133 @@
+"""New user registration form and validation logic."""
+
+from __future__ import annotations
+
+import asyncio
+from collections.abc import AsyncGenerator
+
+import reflex as rx
+
+from .app_state import AppState
+from .login import LOGIN_ROUTE, REGISTER_ROUTE
+import re
+from .supabase_client import supabase_client
+
+
+class RegistrationState(AppState):
+    """Handle registration form submission and redirect to login page after registration."""
+
+    success: bool = False
+    error_message: str = ""
+
+    is_loading: bool = False
+
+    async def handle_registration(
+        self, form_data
+    ) -> AsyncGenerator[rx.event.EventSpec | list[rx.event.EventSpec] | None, None]:
+        """Handle registration form on_submit.
+
+        Set error_message appropriately based on validation results.
+
+        Args:
+            form_data: A dict of form fields and values.
+        """
+
+        # set the following values to spin the button
+        self.is_loading = True
+        yield
+
+        email = form_data["email"]
+        if not email:
+            self.error_message = "email cannot be empty"
+            rx.set_focus("email")
+            # reset state variable again
+            self.is_loading = False
+            yield
+            return
+        if not is_valid_email(email):
+            self.error_message = "email is not a valid email address."
+            rx.set_focus("email")
+            # reset state variable again
+            self.is_loading = False
+            yield
+            return
+
+        password = form_data["password"]
+        if not password:
+            self.error_message = "Password cannot be empty"
+            rx.set_focus("password")
+            # reset state variable again
+            self.is_loading = False
+            yield
+            return
+        if password != form_data["confirm_password"]:
+            self.error_message = "Passwords do not match"
+            [
+                rx.set_value("confirm_password", ""),
+                rx.set_focus("confirm_password"),
+            ]
+            # reset state variable again
+            self.is_loading = False
+            yield
+            return
+
+        # sign up with supabase
+        supabase_client().auth.sign_up(
+            {
+                "email": email,
+                "password": password,
+            }
+        )
+
+        # Set success and redirect to login page after a brief delay.
+        self.error_message = ""
+        self.success = True
+        self.is_loading = False
+        yield
+        await asyncio.sleep(3)
+        yield [rx.redirect(LOGIN_ROUTE), RegistrationState.set_success(False)]
+
+
+@rx.page(route=REGISTER_ROUTE)
+def registration_page() -> rx.Component:
+    """Render the registration page.
+
+    Returns:
+        A reflex component.
+    """
+    register_form = rx.chakra.form(
+        rx.chakra.input(placeholder="email", id="email", type_="email"),
+        rx.chakra.password(placeholder="password", id="password"),
+        rx.chakra.password(placeholder="confirm", id="confirm_password"),
+        rx.chakra.button(
+            "Register",
+            type_="submit",
+            is_loading=RegistrationState.is_loading,
+        ),
+        width="80vw",
+        on_submit=RegistrationState.handle_registration,
+    )
+    return rx.fragment(
+        rx.cond(
+            RegistrationState.success,
+            rx.chakra.vstack(
+                rx.chakra.text(
+                    "Registration successful, check your mail to confirm signup so as to login!"
+                ),
+                rx.chakra.spinner(),
+            ),
+            rx.chakra.vstack(
+                rx.cond(  # conditionally show error messages
+                    RegistrationState.error_message != "",
+                    rx.chakra.text(RegistrationState.error_message),
+                ),
+                register_form,
+                padding_top="10vh",
+            ),
+        )
+    )
+
+
+def is_valid_email(email):
+    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    return re.match(pattern, email) is not None
