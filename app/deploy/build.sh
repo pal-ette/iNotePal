c66a6745633a15b54671a5d0339d244d31894dbc
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-24 11:48:37 +0900

    #40 FE,BE 빌드 스크립트 추가

---
+++ app/deploy/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PYTHON_PATH=$(which python)
+CURRENT_PATH=$(pwd)/.venv/bin/python
+if [ ${PYTHON_PATH} != ${CURRENT_PATH} ] ; then
+    echo "Already inside virtual environment."
+else
+    . $(pwd)/.venv/bin/activate
+fi;
+
+pip install -r requirements.txt
+
+reflex export
