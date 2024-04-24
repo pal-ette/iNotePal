d728b651471bcabb3a812a5b682ffbe4267f1a88
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-24 12:40:05 +0900

    #40 deploy 스크립트 오류 수정

--- app/deploy/build.sh
+++ app/deploy/build.sh
@@ -2,8 +2,8 @@
 
 PYTHON_PATH=$(which python)
 CURRENT_PATH=$(pwd)/.venv/bin/python
-if [ ${PYTHON_PATH} != ${CURRENT_PATH} ] ; then
-    echo "Already inside virtual environment."
+if [ ${PYTHON_PATH} = ${CURRENT_PATH} ] ; then
+    echo "Already inside virtual environment. skip activate"
 else
     . $(pwd)/.venv/bin/activate
 fi;
