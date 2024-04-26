a7c01fb238f27d57f41b1274f56eb6f51fb42ac3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-26 12:41:23 +0900

    #68 ChatState.input_message 처리 제거

--- app/app/component/chat_input.py
+++ app/app/component/chat_input.py
@@ -10,8 +10,6 @@ def chat_input(can_input: bool) -> rx.Component:
                 placeholder="대화를 입력하세요.",
                 id="message",
                 width="100%",
-                value=ChatState.input_message,
-                on_change=ChatState.set_input_message,
                 is_required=True,
                 is_disabled=~can_input,
             ),
@@ -29,5 +27,6 @@ def chat_input(can_input: bool) -> rx.Component:
             width="100%",
         ),
         on_submit=ChatState.on_submit,
+        reset_on_submit=True,
         width="100%",
     )
