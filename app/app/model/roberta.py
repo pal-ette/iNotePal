4dcea7fda06f99c9513caa10b308cca2216fbc97
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-04-29 15:42:24 +0900

    #62 cuda-worker 구현

--- app/app/model/roberta.py
+++ app/app/model/roberta.py
@@ -4,6 +4,8 @@ from torch.autograd import Variable
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from collections import OrderedDict
 from .inference_model import InferenceModel
+import multiprocessing as mp
+import time
 
 
 class RobertaClassifier(nn.Module):
@@ -37,58 +39,8 @@ class RobertaClassifier(nn.Module):
         return out
 
 
-class Roberta(InferenceModel):
-
-    def __init__(self, version) -> None:
-        super().__init__(version)
-
-        self.device = torch.device("cuda:0")
-
-        self.model_name = "klue/roberta-small"
-
-        self.path = f"https://github.com/pal-ette/iNotePal/releases/download/{version}/roberta-small_7-emotions.pt"
-        self.model_path = self.download_file(self.path)
-
-        roberta_model = AutoModelForSequenceClassification.from_pretrained(
-            self.model_name,
-            num_labels=768,
-        )
-
-        roberta_model = RobertaClassifier(roberta_model, 7, 0.2, 768)
-
-        roberta_model = roberta_model.to(self.device)
-
-        self.model, self.label = self.load_dict(self.model_path, roberta_model)
-        print(self.model)
-        print(self.label)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-
-    def tokenize(self, string):
-
-        string = re.sub(r"[^가-힣ㅏ-ㅣㄱ-ㅎA-Za-z0-9,!?]", " ", string)
-
-        return self.tokenizer(
-            string,
-            truncation=True,
-            padding="max_length",
-            max_length=64,
-            return_tensors="pt",
-        )
-
-    def padding(self, tokenzied_string):
-        return tokenzied_string
-
-    def predict(self, sentence):
-        attention_mask = sentence["attention_mask"].to(self.device)
-        input_ids = sentence["input_ids"].squeeze(1).to(self.device)
-
-        out = self.model(input_ids, attention_mask)  # [0]
-        max_vals, max_indices = torch.max(out, 1)
-
-        prediction = self.label[max_indices]  # predicted emotion in Korean
-        return prediction
-
-    def load_dict(self, file_path, model):
+def cuda_worker(model_name, model_path, input, output):
+    def load_dict(file_path, model):
 
         best_model = torch.load(file_path)
 
@@ -104,3 +56,72 @@ class Roberta(InferenceModel):
         model.load_state_dict(model_dict)
 
         return model, label
+
+    device = torch.device("cuda:0")
+
+    roberta_model = AutoModelForSequenceClassification.from_pretrained(
+        model_name,
+        num_labels=768,
+    )
+
+    roberta_model = RobertaClassifier(roberta_model, 7, 0.2, 768)
+
+    roberta_model = roberta_model.to(device)
+
+    model, label = load_dict(model_path, roberta_model)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    for _func, sentence in iter(input.get, "STOP"):
+        sentence = re.sub(r"[^가-힣ㅏ-ㅣㄱ-ㅎA-Za-z0-9,!?]", " ", sentence)
+
+        sentence = tokenizer(
+            sentence,
+            truncation=True,
+            padding="max_length",
+            max_length=64,
+            return_tensors="pt",
+        )
+
+        attention_mask = sentence["attention_mask"].to(device)
+        input_ids = sentence["input_ids"].squeeze(1).to(device)
+
+        out = model(input_ids, attention_mask)  # [0]
+        max_vals, max_indices = torch.max(out, 1)
+
+        prediction = label[max_indices]  # predicted emotion in Korean
+        output.put(prediction)
+
+
+class Roberta(InferenceModel):
+
+    def __init__(self, version) -> None:
+        super().__init__(version)
+
+        self.model_name = "klue/roberta-small"
+
+        self.path = f"https://github.com/pal-ette/iNotePal/releases/download/{version}/roberta-small_7-emotions.pt"
+        self.model_path = self.download_file(self.path)
+
+        self.input_queue = mp.Queue()
+        self.output_queue = mp.Queue()
+
+        cuda_process = mp.Process(
+            target=cuda_worker,
+            args=(
+                self.model_name,
+                self.model_path,
+                self.input_queue,
+                self.output_queue,
+            ),
+        ).start()
+
+    def tokenize(self, string):
+        return string
+
+    def padding(self, tokenzied_string):
+        return tokenzied_string
+
+    def predict(self, sentence):
+        self.input_queue.put((0, sentence))
+        return self.output_queue.get()
