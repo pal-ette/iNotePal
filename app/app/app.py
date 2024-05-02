f3264e367b70099af8663e33326ea02aa853f824
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-05-02 15:11:55 +0900

    #144 자바 캐시에 저장하도록 수정

--- app/app/app.py
+++ app/app/app.py
@@ -21,8 +21,14 @@ import jdk
 
 load_dotenv()
 
-jdk_dir = jdk.install("22")
-print(f"JAVA INSTALLED: {jdk_dir}")
+jdk_dir = f"{os.getcwd()}/.cache/jdk"
+if os.path.isdir(jdk_dir):
+    [first_version] = os.listdir(jdk_dir)
+    jdk_dir = f"{jdk_dir}/{first_version}"
+    print(f"JAVA LOADED: {jdk_dir}")
+else:
+    jdk_dir = jdk.install("22", path=f"{os.getcwd()}/.cache/jdk")
+    print(f"JAVA INSTALLED: {jdk_dir}")
 os.environ["JAVA_HOME"] = jdk_dir
 os.environ["PATH"] = f"{os.environ.get('PATH')}:{jdk_dir}/bin"
 
