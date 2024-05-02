16b0be75be231c734adbab3d123c29fd9b388786
momingyu <103172274+doctorpeppercokebear@users.noreply.github.com>
2024-05-02 11:17:24 +0900

    #122 코드 주석 추가

--- app/app/page/calendar.py
+++ app/app/page/calendar.py
@@ -5,7 +5,7 @@ from app.state.calendar_state import State
 
 cal = calendar.Calendar()
 
-# define styles
+# 캘린더 셀 스타일 정의
 cal_days_style = {
     "width": "50px",
     "height": "50px",
@@ -14,6 +14,7 @@ cal_days_style = {
     "justify_content": "center",
     "border_radius": "6px",
 }
+# 캘린더 행 스타일 정의
 cal_row_style = {
     "width": "50px",
     "height": "50px",
@@ -24,7 +25,7 @@ cal_row_style = {
 }
 
 
-# define method to return text for days
+# 각 캘린더 셀 생성 함수, 날짜, 요일, 스타일 정보를 매겨변수로 받아 셀 요소로 반환
 def calendar_days(data):
     return rx.container(
         rx.text(
@@ -37,7 +38,7 @@ def calendar_days(data):
     )
 
 
-#  define method to return grid row
+#  요일과 날짜를 포함하는 캘린더 행을 생성 함수
 def calendar_grid_row(data: list[str]):
     date_str = data[0]
     return rx.container(
@@ -60,17 +61,19 @@ def calendar_grid(data):
     )
 
 
-# Reflex page definition
+# Reflex 화면 정의
 @rx.page(route="/calendar", on_load=State.get_calendar_data)
 def index() -> rx.Component:
-    return rx.vstack(
+    # vstack : 자식 요소(달/연 이동 버튼, 요일 표시, 캘린더 그리드)들을 세로로 쌓아 배치
+    return rx.vstack(                   
         # calendar start ...
+        # hsatack : 자식 요소들을 가로로 배치
         rx.hstack(
             rx.icon(
                 tag="chevron_left", cursor="pointer", on_click=State.delta_calendar(-1)
             ),
-            rx.spacer(),
-            rx.text(
+            rx.spacer(),        # 빈 공간 생성
+            rx.text(            # 현재 월과 연도를 표시하는 텍스트
                 f"{State.month_class[State.month]} {State.year}",
                 width="150px",
                 display="flex",
--- app/app/state/calendar_state.py
+++ app/app/state/calendar_state.py
@@ -7,33 +7,35 @@ cal = calendar.Calendar()
 
 
 # class_State 분리
+# 캘린더 UI, 연도, 월, 선택된 날짜, 캘린더 그리드 데이터 관리하는 클래스
 class State(rx.State):
-    year: int = datetime.datetime.now().year
-    month: int = datetime.datetime.now().month
+    year: int = datetime.datetime.now().year    # 연도 저장
+    month: int = datetime.datetime.now().month  # 표시할 월 저장
 
-    calendar_data: list[list[str]]
+    calendar_data: list[list[str]]      # 데이터 저장 변수
 
-    # 날짜 선택
+    # 현재 선택된 날짜를 저장하는 변수 지정, 선택전에는 None 상태
     selected_date: str | None = None  # Add to track selected date
-
+    # 날짜 변경시 호출되는 콜백함수 저장
     _on_change = None
 
-    #
+    # 날짜 선택되었을 때 호출되는 메서드
     def select_date(self, date: str) -> None:
         self.selected_date = date
         if self._on_change:
             self._on_change(date)
-        self.get_calendar_data()
+        self.get_calendar_data()        # 함수 호출해 선택된 날짜 반영하여 캘린더 그리드 재 생성
 
-    # define a method to clear calendar data
+    # 캘린더 그리드의 데이터 초기화
     def clear_calendar_grid(self):
         self.calendar_data = []
 
-    # define a method to populate the grid
+    # 그리드를 채우는 방법 정의
+    # python의 calendar 모듈 활용하여 선택된 연도와 월에 대한 캘린더 데이터 생성
     def get_calendar_data(self):
-        self.clear_calendar_grid()
+        self.clear_calendar_grid()      # 그리드 함수 호출 하여  기존 그리드 데이터 초기화
         print(self.selected_date)
-        for week in cal.monthdayscalendar(self.year, self.month):
+        for week in cal.monthdayscalendar(self.year, self.month):       # 월별 일정 데이터 가져온다
             temp_list: list = []
             for day in week:
                 if day == 0:
@@ -72,15 +74,17 @@ class State(rx.State):
         6: "일",
     }
 
-    # define method to change month (and eyar)
+    # define method to change month (and year)
+    # 이전달 또는 다음달로 이동하는 기능을 수행
     def delta_calendar(self, delta: int):
-        if delta == 1:
-            if self.month + delta > 12:
+        if delta == 1:          
+            if self.month + delta > 12:     # # 12월을 넘어가면, 연도를 +1하고, 월을 1로 설정
                 self.month = 1
                 self.year += 1
             else:
                 self.month += 1
-
+        
+        # 1월 이전이면, 연도를 -1하고, 월을 12로 설정
         if delta == -1:
             if self.month + delta < 1:
                 self.month = 12
