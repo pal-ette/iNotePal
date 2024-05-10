e1653b607efe67016e4feae2b3f8be337d42c9a3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-05-10 15:05:31 +0900

    #183 options 하드코딩 제거

--- app/app/component/word_cloud.py
+++ app/app/component/word_cloud.py
@@ -1,13 +1,16 @@
 import reflex as rx
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
 class WordCloud(rx.Component):
     """WordCloud component."""
 
-    tag = "ReactWordcloud options={options}"
+    tag = "ReactWordcloud"
 
     words: rx.Var[List[Dict[str, str | int]]] = []
+
+    options: rx.Var[Dict[str, Any]] = {}
+
     is_default = True
 
     lib_dependencies: List[str] = [
@@ -19,20 +22,7 @@ class WordCloud(rx.Component):
 const ReactWordcloud = dynamic(() => import('react-wordcloud'), {
   ssr: false,
 })
-const options = {
-  colors: ["#49312d", "#91615a", "#af625c", "#de776c", "#e5988e", "#ebb9b0", "#f2ebc8"],
-  rotations: 2,
-  rotationAngles: [-90, 0],
-  fontFamily: "impact",
-  padding: 1,
-  scale: "sqrt",
-  fontSizes: [10, 60],
-  fontStyle: "normal",
-}
 """
-
-
-# const size = [600, 600]
 
 
 word_cloud = WordCloud.create
