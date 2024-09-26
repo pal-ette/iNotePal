555b3ba927d3215c43c19e8884da9948731b5d86
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-09-26 23:17:31 +0900

    #217 내부 DB 쿼리시 에러 수정

--- app/app/schema/chat.py
+++ app/app/schema/chat.py
@@ -2,6 +2,7 @@ import reflex as rx
 import sqlmodel
 from datetime import date as date_type
 from datetime import datetime as time_type
+from typing import List, Optional
 from .emotion import Emotion
 
 
@@ -15,3 +16,24 @@ class Chat(rx.Model, table=True):
     created_at: time_type
 
     emotion: Emotion
+
+    messages: List["Message"] = sqlmodel.Relationship(
+        back_populates="chat",
+    )
+
+
+class Message(rx.Model, table=True):
+
+    created_at: time_type
+
+    chat_id: int = sqlmodel.Field(foreign_key="chat.id")
+
+    message: str
+
+    is_user: bool
+
+    emotion: Emotion
+
+    chat: Optional["Chat"] = sqlmodel.Relationship(
+        back_populates="messages",
+    )
