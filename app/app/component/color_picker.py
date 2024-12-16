176efb3f3bfa1750eee07edcd4e89dfc0d97613e
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-12-16 23:53:44 +0900

    #309 컬러픽커 on_change 이벤트 타입 명시

--- app/app/component/color_picker.py
+++ app/app/component/color_picker.py
@@ -1,11 +1,12 @@
 import reflex as rx
+from reflex.event import passthrough_event_spec
 
 
 class ColorPicker(rx.Component):
     library = "react-colorful"
     tag = "HexColorPicker"
     color: rx.Var[str]
-    on_change: rx.EventHandler[lambda color: [color]]
+    on_change: rx.EventHandler[passthrough_event_spec(str)]
 
 
 color_picker = ColorPicker.create
