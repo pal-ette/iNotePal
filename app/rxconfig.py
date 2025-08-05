9eae57bc1bd63de295a7780961f74504d50a9a7b
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-08-05 17:48:33 +0900

    #389 reflex 바뀐 config 구조 대응

--- app/rxconfig.py
+++ app/rxconfig.py
@@ -8,42 +8,28 @@ import dotenv
 backend_port = 8080
 frontend_port = 3000
 
+custom_config = {
+    "app_name": "app",
+    "backend_port": backend_port,
+    "frontend_port": frontend_port,
+    "api_url": f"http://localhost:{backend_port}",
+    "env_file": ".env",
+}
 
-class BaseConfg(rx.Config):
-    app_name: str = "app"
-
-    backend_port: int = backend_port
-
-    frontend_port: int = frontend_port
-
-    api_url: str = f"http://localhost:{backend_port}"
-
-    env_file: Optional[str] = ".env"
-
-
-class DevConfig(BaseConfg):
-    cors_allowed_origins: List[str] = [
-        f"http://localhost:{frontend_port}",
-    ]
-
-
-class ProdConfig(BaseConfg):
-    cors_allowed_origins: List[str] = [
-        "https://pal-ette.github.io",
-    ]
-
-    api_url: str = "https://api.pal-ette.org"
-
-    deploy_url: Optional[str] = "https://pal-ette.github.io/iNotePal"
-
-    frontend_path: str = "/iNotePal"
-
-    db_url = f"postgresql://{os.getenv('POSTGRESQL_USER')}:{os.getenv('POSTGRESQL_PW')}@{os.getenv('POSTGRESQL_HOST')}/postgres"
-
-
-config = BaseConfg()
 env = environment.REFLEX_ENV_MODE.get()
 if env == constants.Env.DEV:
-    config = DevConfig()
+    custom_config["cors_allowed_origins"] = [
+        f"http://localhost:{frontend_port}",
+    ]
 elif env == constants.Env.PROD:
-    config = ProdConfig()
+    custom_config["cors_allowed_origins"] = [
+        "https://pal-ette.github.io",
+    ]
+    custom_config["api_url"] = "https://api.pal-ette.org"
+    custom_config["deploy_url"] = "https://pal-ette.github.io/iNotePal"
+    custom_config["frontend_path"] = "/iNotePal"
+    custom_config["db_url"] = (
+        f"postgresql://{os.getenv('POSTGRESQL_USER')}:{os.getenv('POSTGRESQL_PW')}@{os.getenv('POSTGRESQL_HOST')}/postgres"
+    )
+
+config = rx.Config(**custom_config)
